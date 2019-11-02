344229d054e7744517a50e297a401874821be9aa
liulinboyi <814921718@qq.com>
2019-11-02 14:04:15 +0800

    顶部菜单完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-
+interface topMenu {
+  title: string;
+  link?: string;
+}interface addFunction {
+  (x: number,y: number):number
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,81 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pinduoduo';
+  selectedIndex = -1;
+  handleSelection(i) {
+    this.selectedIndex = i
+  }
+  menus: Array<topMenu> = [
+    {
+      title: '热门',
+      link: ''
+    },
+    {
+      title: '男装',
+      link: ''
+    },
+    {
+      title: '百货',
+      link: ''
+    },
+    {
+      title: '运动',
+      link: ''
+    },
+    {
+      title: '手机',
+      link: ''
+    },
+    {
+      title: '家纺',
+      link: ''
+    },
+    {
+      title: '食品',
+      link: ''
+    },
+    {
+      title: '电器',
+      link: ''
+    },
+    {
+      title: '鞋包',
+      link: ''
+    },
+    {
+      title: '汽车',
+      link: ''
+    },
+    {
+      title: '水果',
+      link: ''
+    },
+    {
+      title: '电脑',
+      link: ''
+    },
+    {
+      title: '内衣',
+      link: ''
+    },
+    {
+      title: '家装',
+      link: ''
+    },
+    {
+      title: '母婴',
+      link: ''
+    },
+    {
+      title: '美妆',
+      link: ''
+    },
+    {
+      title: '家具',
+      link: ''
+    }
+  ]
+  add(x,y) {
+    return x + y
+  }
 }
